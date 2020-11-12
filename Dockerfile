94b3f4aa19999677c0d9bb5803f7220c15f5567e
ohchang-kwon <ohchang.kwon@navercorp.com>
2020-11-12 20:38:07 +0900

    fix Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,11 @@
-FROM golang:1.13-buster
+FROM git AS fuse-overlayfs-test-src
+WORKDIR /usr/src
+RUN git clone https://github.com/shortstories/fuse-overlayfs-test.git fuse-overlayfs-test
+
+FROM golang:1.14-buster
 
 WORKDIR /build
-COPY . .
-
+RUN --mount=from=fuse-overlayfs-test-src,src=/usr/src/fuse-overlayfs-test,target=.,readwrite \
 RUN go build -o fuse-overlayfs-test . && \
     ./fuse-overlayfs-test && \
     ls -al
--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-
+# moby/buildkit:master-rootless \
 docker run \
     -it \
     --rm \
     --privileged \
-    -e BUILDKITD_FLAGS='--oci-worker-no-process-sandbox --oci-worker-snapshotter=fuse-overlayfs' \
+    -e BUILDKITD_FLAGS='--root=/tmp/.local/share/buildkit --oci-worker-no-process-sandbox --oci-worker-snapshotter=fuse-overlayfs' \
     -v $(pwd):/tmp/work \
     --entrypoint buildctl-daemonless.sh \
-    moby/buildkit:master-rootless \
+    reg.navercorp.com/ncc-bd/buildkit:master-20201112-1 \
         build \
         --frontend \
         dockerfile.v0 \
